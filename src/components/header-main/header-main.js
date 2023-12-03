d4c6f0eb787a86e8ac043ce1d1d8669ce94a2f2e
Yernur <220776@astanait.edu.kz>
2023-12-03 15:36:47 +0600

    used bootstrap components in header-main component

--- src/components/header-main/header-main.js
+++ src/components/header-main/header-main.js
@@ -1,17 +1,18 @@
 import './header-main.scss'
 import LogoBeans from "../logo-beans/logo-beans";
+import {Button, Container} from "react-bootstrap";
 
 const HeaderMain = () => {
 
     return (
         <header className="header-main w-100 text-white">
-            <div className="container text-center">
+            <Container className={'text-center'}>
                 <h1 className={'header-main__title'}>Everything You Love About Coffee</h1>
                 <LogoBeans type={'white'} />
                 <p className="header-main__content">We makes every day full of energy and taste</p>
                 <p className="header-main__try">Want to try our beans?</p>
-                <button className="header-main__more">More</button>
-            </div>
+                <Button variant={'outline-light'} className={'header-main__more'}>More</Button>
+            </Container>
         </header>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './style/index.scss';
+
 import App from './components/app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

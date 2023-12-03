29c82d72fad5bd5d483296207636df4f30e2c4ae
Yernur <220776@astanait.edu.kz>
2023-12-04 01:34:35 +0600

    updated header-mini
    * use props for header text

--- src/components/header-mini/header-mini.js
+++ src/components/header-mini/header-mini.js
@@ -2,12 +2,13 @@ import {Container} from "react-bootstrap";
 
 import './header-mini.scss'
 
-const HeaderMini = () => {
+const HeaderMini = (props) => {
+    const {text} = props
 
     return (
         <header className="header-mini w-100 text-white text-center">
             <Container>
-                <p className="header-mini__content">Our Coffee</p>
+                <p className="header-mini__content">{text}</p>
             </Container>
         </header>
     )
